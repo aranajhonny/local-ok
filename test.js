aed64120eeaba9cdc396c3defe15e127ddbbef08
aranajhonny <jhonnyjosearana@gmail.com>
2016-09-25 13:56:40 -0400

    badge

--- test.js
+++ test.js
@@ -2,12 +2,12 @@ import test from 'ava';
 import http from 'ava-http';
 import lok from './index'
 
-test('response headers', async t => {
+test('response example page', async t => {
 	lok.server({
 	  folder:'example',
 	  port: '3000'
 	});
     const res = await http.getResponse('http://127.0.0.1:3000');
-    console.log('Get page');
+    console.log('Get page index.html');
     t.is(res.statusCode, 200);
 });
