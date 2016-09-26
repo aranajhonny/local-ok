09051c1ac7e3bd2a39cb9bdbc8aca471ca4d4a44
aranajhonny <jhonnyjosearana@gmail.com>
2016-09-25 22:32:12 -0400

    add test ava

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
+'use strict';
 const http = require('http');
-const static = require('serve-static')
+const estatic = require('serve-static');
 const chalk = require('chalk');
 const finalhandler = require('finalhandler');
 const compression = require('compression');
 
-const  PORT = process.env.PORT || '3000';
-const  FOLDER = 'public'
+const PORT = process.env.PORT || '3000';
+const FOLDER = 'public';
 
-exports.server = (options) => {
-	options = options || {}
-	let port = options.port || process.env.PORT || PORT
-	let folder = options.folder || FOLDER
-	let serve = static(folder);
-	let server = http.createServer(function onRequest (req, res) {
-		let _compress = compression()
-		 _compress(req, res, function (err) {
-	  		  serve(req, res, finalhandler(req, res))
-	  	 });
+exports.server = options => {
+	options = options || {};
+	const port = options.port || process.env.PORT || PORT;
+	const folder = options.folder || FOLDER;
+	const serve = estatic(folder);
+	const server = http.createServer(function onRequest(req, res) {
+		const _compress = compression();
+		_compress(req, res, () => {
+			serve(req, res, finalhandler(req, res));
+		});
 	});
-	server.listen(port)
-	console.log( chalk.green(`Listening ${folder} on 127.0.0.1:${port}`));
-
-}
+	server.listen(port);
+	console.log(chalk.green(`Listening ${folder} on 127.0.0.1:${port}`));
+};
 
--- test.js
+++ test.js
@@ -1,13 +1,24 @@
 import test from 'ava';
 import http from 'ava-http';
-import lok from './index'
+import lok from './index';
 
-test('response example page', async t => {
+test('response 200', async t => {
 	lok.server({
-	  folder:'example',
-	  port: '3000'
+		folder: 'example',
+		port: '3001'
 	});
-    const res = await http.getResponse('http://127.0.0.1:3000');
-    console.log('Get page index.html');
-    t.is(res.statusCode, 200);
+	const res = await http.getResponse('http://127.0.0.1:3001');
+	t.is(res.statusCode, 200);
+	t.pass();
+});
+
+test('response 404', async t => {
+	lok.server({
+		folder: '',
+		port: '3002'
+	});
+	http.getResponse('http://127.0.0.1:3002').catch(err => {
+		t.is(err.statusCode, 404);
+		t.pass();
+	});
 });
